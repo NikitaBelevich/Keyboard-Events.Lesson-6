2e4bc1311eac14a93b8fc52cfe15df2262e8c35a
Nikita Belevich <belevish_1998@mail.ru>
2020-07-02 22:18:46 +0300

    Task 3 complete

--- js/app.js
+++ js/app.js
@@ -25,3 +25,16 @@ input2.addEventListener('keydown', (e) => {
 function t2(event) {
     return event.keyCode;
 }
+
+// Task 3 ============================================
+/*  Дан input .i-3. Напишите функцию t3, которая выводит на страницу true если введен символ и false если цифра. Для определения - используйте код клавиши. */
+const input3 = document.querySelector('.i-3');
+const out3 = document.querySelector('.out-3');
+
+input3.addEventListener('keydown', (e) => {
+    out3.textContent = t3(e);
+});
+
+function t3(event) {
+    return (!event.code.includes('Digit')) ? true : false;
+}
